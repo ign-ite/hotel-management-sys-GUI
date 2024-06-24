022021690c8d7a9064ef15913cfd86440d503f98
Varun Kumar <varuntheace@gmail.com>
2024-06-24 16:08:36 +0530

    Update listgui.py

--- listgui.py
+++ listgui.py
@@ -54,7 +54,7 @@ except ImportError:
 class HOTEL_MANGMENT_checkin:
     def __init__(self):
         root = Tk()
-        _bgcolor = '#d9d9d9'  
+        _bgcolor = '#d9d9d9'
         _fgcolor = '#000000'
         _compcolor = '#ffffff'
         _ana1color = '#ffffff'
@@ -94,3 +94,88 @@ class HOTEL_MANGMENT_checkin:
         self.Frame1.configure(highlightbackground="#ffffff")
         self.Frame1.configure(highlightcolor="black")
         self.Frame1.configure(width=355)
+
+        self.Label1 = Label(self.Frame1)
+        self.Label1.place(relx=0.28, rely=0.02, height=37, width=117)
+        self.Label1.configure(activebackground="#ffffff")
+        self.Label1.configure(activeforeground="black")
+        self.Label1.configure(background="#d9d9d9")
+        self.Label1.configure(disabledforeground="#a3a3a3")
+        self.Label1.configure(font=font11)
+        self.Label1.configure(foreground="#000000")
+        self.Label1.configure(highlightbackground="#ffffff")
+        self.Label1.configure(highlightcolor="black")
+        self.Label1.configure(text='''NAMES''')
+
+        self.Text1 = Text(self.Frame1)
+        self.Text1.place(relx=0.06, rely=0.16, relheight=0.8, relwidth=0.88)
+        self.Text1.configure(background="white")
+        self.Text1.configure(font=font14)
+        self.Text1.configure(foreground="#000000")
+        self.Text1.configure(highlightbackground="#d9d9d9")
+        self.Text1.configure(highlightcolor="black")
+        self.Text1.configure(insertbackground="black")
+        self.Text1.configure(selectbackground="#c4c4c4")
+        self.Text1.configure(selectforeground="black")
+        self.Text1.configure(width=314)
+        self.Text1.configure(wrap=WORD)
+
+        self.Frame2 = Frame(self.Labelframe1)
+        self.Frame2.place(relx=0.51, rely=0.1, relheight=0.86, relwidth=0.47
+                          , y=-31, h=15)
+        self.Frame2.configure(relief=GROOVE)
+        self.Frame2.configure(borderwidth="2")
+        self.Frame2.configure(relief=GROOVE)
+        self.Frame2.configure(background="#d9d9d9")
+        self.Frame2.configure(highlightbackground="#ffffff")
+        self.Frame2.configure(highlightcolor="black")
+        self.Frame2.configure(width=355)
+
+        self.Label2 = Label(self.Frame2)
+        self.Label2.place(relx=0.37, rely=0.02, height=44, width=152)
+        self.Label2.configure(activebackground="#ffffff")
+        self.Label2.configure(activeforeground="black")
+        self.Label2.configure(background="#d9d9d9")
+        self.Label2.configure(disabledforeground="#a3a3a3")
+        self.Label2.configure(font=font11)
+        self.Label2.configure(foreground="#000000")
+        self.Label2.configure(highlightbackground="#ffffff")
+        self.Label2.configure(highlightcolor="black")
+        self.Label2.configure(text='''ROOM NO.''')
+
+        self.Text2 = Text(self.Frame2)
+        self.Text2.place(relx=0.06, rely=0.16, relheight=0.8, relwidth=0.88)
+        self.Text2.configure(background="white")
+        self.Text2.configure(font=font14)
+        self.Text2.configure(foreground="black")
+        self.Text2.configure(highlightbackground="#d9d9d9")
+        self.Text2.configure(highlightcolor="black")
+        self.Text2.configure(insertbackground="black")
+        self.Text2.configure(selectbackground="#c4c4c4")
+        self.Text2.configure(selectforeground="black")
+        self.Text2.configure(width=314)
+        self.Text2.configure(wrap=WORD)
+        for i in range(0, len(G)):
+            s = str(l2[i])
+            h = str(G[i])
+            self.Text1.insert(INSERT, s + "\n")
+            self.Text2.insert(INSERT, h + "\n")
+
+        root.mainloop()
+
+if __name__ == '__main__':
+    f2 = open("hotel.dat", "rb")
+    try:
+        while True:
+            s = pickle.load(f2)
+            k = s.room_no
+            o = s.name.upper()
+            l2.append(o)
+
+            G.append(k)
+            continue
+
+    except EOFError:
+        pass
+    f2.close()
+    hotel = HOTEL_MANGMENT_checkin()
